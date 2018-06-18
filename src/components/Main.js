240de38938825977cc06821f65c2d1a1e7058be9
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-06-17 20:23:56 -0400

    address security risk

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,10 +7,10 @@ export default () => {
       <h1> Phill Ayers </h1>
       <h1 className="sub"> SOFTWARE ENGINEER </h1>
       <div className="icons">
-        <a href='https://github.com/payers1' target="_blank">
+        <a href='https://github.com/payers1' target="_blank" rel="noopener noreferrer">
           <Icon type="github" />
         </a>
-        <a href='https://www.linkedin.com/in/phill-ayers-aa6074b8' target="_blank">
+        <a href='https://www.linkedin.com/in/phill-ayers-aa6074b8' target="_blank" rel="noopener noreferrer">
           <Icon type="linkedin" />
         </a>
       </div>
