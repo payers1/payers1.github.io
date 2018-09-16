2f5f616daee40c1d69a9bee2fafda7f60512e9e4
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-09-16 15:52:37 -0400

    Add wutang

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import { Icon } from 'antd';
+import React from "react";
+import { Icon } from "antd";
 
-export default () => {
-  return <div className='Main'>
+export default () => (
+  <div className="Main">
     <div>
       <h1> Phill Ayers </h1>
       <h1 className="sub"> SOFTWARE ENGINEER </h1>
       <div className="icons">
-        <a href='https://github.com/payers1' target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/payers1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Icon type="github" />
         </a>
-        <a href='https://www.linkedin.com/in/phill-ayers-aa6074b8' target="_blank" rel="noopener noreferrer">
+        <img src="/wu.jpeg" height="50" />
+        <a
+          href="https://www.linkedin.com/in/phill-ayers-aa6074b8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Icon type="linkedin" />
         </a>
       </div>
-
     </div>
   </div>
-
-}
+);
