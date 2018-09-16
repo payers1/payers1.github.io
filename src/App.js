991d4c4f9b8f969315de430702ae6b018860a51e
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-09-16 16:07:02 -0400

    refactor Map into separate component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,20 @@
-import React, { Component } from 'react'
-import ReactMapGL from 'react-map-gl'
+import React from 'react'
 import { Row, Col } from 'antd'
-import { delay } from 'bluebird'
 import './App.css'
 import Main from './components/Main'
+import Map from './components/Map'
 
-const MAPSTYLE = 'mapbox://styles/mapbox/dark-v9'
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      viewport: {
-        width: window.innerWidth / 2,
-        height: window.innerHeight,
-        latitude: 43.15003802563228,
-        longitude: -77.58613866914878,
-        zoom: 5
-      }
-    }
-    this.map = React.createRef()
-  }
-
-  componentDidMount() {
-    const map = this.map.current.getMap()
-    map.on('load', async () => {
-      await delay(1500)
-      this.setState({
-        viewport: {
-          ...this.state.viewport,
-          zoom: 13,
-          transitionDuration: 20000
-        }
-      })
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Row type="flex" justify="space-around" align="middle">
-          <Col xs={24} md={12}>
-            <Main />
-          </Col>
-          <Col xs={24} md={12}>
-            <ReactMapGL
-              {...this.state.viewport}
-              ref={this.map}
-              reuseMaps={true}
-              mapStyle={MAPSTYLE}
-              mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-              onViewportChange={viewport => this.setState({ viewport })}
-            />
-          </Col>
-        </Row>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className="App">
+    <Row type="flex" justify="space-around" align="middle">
+      <Col xs={24} md={12}>
+        <Main />
+      </Col>
+      <Col xs={24} md={12}>
+        <Map />
+      </Col>
+    </Row>
+  </div>
+)
 
 export default App
---
+++ src/components/Map.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import ReactMapGL from 'react-map-gl'
+import { delay } from 'bluebird'
+
+const MAPSTYLE = 'mapbox://styles/mapbox/dark-v9'
+
+class Map extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      viewport: {
+        width: window.innerWidth / 2,
+        height: window.innerHeight,
+        latitude: 43.15003802563228,
+        longitude: -77.58613866914878,
+        zoom: 5
+      }
+    }
+    this.map = React.createRef()
+  }
+
+  componentDidMount() {
+    const map = this.map.current.getMap()
+    map.on('load', async () => {
+      await delay(1500)
+      this.setState({
+        viewport: {
+          ...this.state.viewport,
+          zoom: 13,
+          transitionDuration: 20000
+        }
+      })
+    })
+  }
+
+  render() {
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        ref={this.map}
+        reuseMaps={true}
+        mapStyle={MAPSTYLE}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={viewport => this.setState({ viewport })}
+      />
+    )
+  }
+}
+
+export default Map
