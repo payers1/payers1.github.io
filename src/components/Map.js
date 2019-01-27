6524f9fdc29c307537d14e43d2def75bee41f07e
Phill Ayers <phillip.ayers@sharpnotions.com>
2019-01-26 19:25:32 -0500

    Refactor map to use react hooks

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,53 +1,36 @@
-import React, { Component } from 'react'
+import React, { useReducer } from 'react'
 import ReactMapGL, { HTMLOverlay } from 'react-map-gl'
-import { delay } from 'bluebird'
 import LocationOverlay from './LocationOverlay'
 
 const MAPSTYLE = 'mapbox://styles/mapbox/dark-v9'
 
-class Map extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      viewport: {
-        width: window.innerWidth / 2,
-        height: window.innerHeight,
-        latitude: 43.15003802563228,
-        longitude: -77.58613866914878,
-        zoom: 5
-      }
-    }
-    this.map = React.createRef()
+const initialState = {
+  viewport: {
+    width: window.innerWidth / 2,
+    height: window.innerHeight,
+    latitude: 43.15003802563228,
+    longitude: -77.58613866914878,
+    zoom: 5
   }
+}
 
-  componentDidMount() {
-    const map = this.map.current.getMap()
-    map.on('load', async () => {
-      // await delay(1500)
-      // this.setState({
-      //   viewport: {
-      //     ...this.state.viewport,
-      //     zoom: 13,
-      //     transitionDuration: 20000
-      //   }
-      // })
-    })
-  }
+function Map({ showOverLay }) {
+  const [state, setState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    initialState
+  )
 
-  render() {
-    return (
-      <ReactMapGL
-        {...this.state.viewport}
-        ref={this.map}
-        reuseMaps={true}
-        mapStyle={MAPSTYLE}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        onViewportChange={viewport => this.setState({ viewport })}
-      >
-        <HTMLOverlay redraw={props => <LocationOverlay />} />
-      </ReactMapGL>
-    )
-  }
+  return (
+    <ReactMapGL
+      {...state.viewport}
+      reuseMaps={true}
+      mapStyle={MAPSTYLE}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={viewport => setState({ viewport })}
+    >
+      {showOverLay && <HTMLOverlay redraw={props => <LocationOverlay />} />}
+    </ReactMapGL>
+  )
 }
 
 export default Map
